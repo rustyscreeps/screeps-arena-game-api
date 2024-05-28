ad3459f1aba9f686fa0a9bfe8790b2b764e0fe3e
Shane Madden <shanemadden@users.noreply.github.com>
2024-05-27 18:20:16 -0600

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
 // to build locally with doc_cfg enabled, run:
 // `RUSTDOCFLAGS="--cfg docsrs" cargo +nightly doc --all-features`
 #![cfg_attr(docsrs, feature(doc_cfg))]
-
 // temporary workaround for https://github.com/rust-lang/rust-clippy/issues/12377
 // fix not being in current stable rust 1.78; should be fixed in 1.79
 #![allow(clippy::empty_docs)]
