3db57453bbc1a1eb226601173e3829715e33ce2d
gulbanana <1612285+gulbanana@users.noreply.github.com>
2026-03-19 00:35:47 +0800

    fix: GameObject.findPathTo returns an array, not a SearchResults (#21)

--- src/traits.rs
+++ src/traits.rs
@@ -3,7 +3,7 @@ use js_sys::{Array, JsString, Object};
 
 use crate::{
     enums::*,
-    game::pathfinder::{FindPathOptions, Position, SearchResults},
+    game::pathfinder::{FindPathOptions, Position},
     objects::*,
 };
 
@@ -167,7 +167,7 @@ pub trait GameObjectProperties {
     /// If defined, then this object will disappear after this number of ticks.
     fn ticks_to_decay(&self) -> Option<u32>;
 
-    fn find_path_to(&self, pos: &Object, options: Option<&FindPathOptions>) -> SearchResults;
+    fn find_path_to(&self, pos: &Object, options: Option<&FindPathOptions>) -> Array;
 
     fn find_in_range(&self, positions: &Array, range: u8) -> Array;
 
